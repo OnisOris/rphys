052240e6dc0e4eeeb8a5f4bd7d67c197d9775880
Michael <onisoris@yandex.ru>
2025-11-17 00:27:40 +0300

    Add: flocking.rs FlockAlgorithm, FlockBehavior

---
+++ src/flocking.rs
@@ -0,0 +1,21 @@
+use nginphy::{BodyConfig, Point, Simulator};
+
+pub enum FlockAlgorithm {
+    Basic,
+    WithGamma,
+    WithObstacles,
+}
+
+pub struct FlockingBehavior {
+    pub algorithm: FlockAlgorithm,
+    pub eps: f64,
+    pub r: f64,
+    pub c_alpha1: f64,
+    pub c_alpha2: f64,
+    pub c_beta1: f64,
+    pub c_beta2: f64,
+    pub c_gamma1: f64,
+    pub c_gamma2: f64,
+    pub gamma: Option<(Vector3<f64>, Vector3<f64>)>,
+    pub obstacles: Vec<Obstacle>, // center+radius
+}
